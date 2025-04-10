caa509d054c667f18504d338cec0ceb27a19629b
yohannes2025 <johannes.m.tekle@gmail.com>
2025-04-10 08:49:38 +0200

    models.py: Define Category, Priority, TaskStatus, Task, UserProfile, Settings, and Attachment models

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -13,3 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import path, include
+from productivity_app import urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('productivity_app.urls')),
+]
